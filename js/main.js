932de7d740452ea20c2108cbdd3cc4c9e621565d
jsap1904 <sapinoso.j@gmail.com>
2017-06-06 20:50:26 -0700

    update

--- js/main.js
+++ js/main.js
@@ -3,34 +3,39 @@ function instaSearch(){
 	document.getElementById('results').innerHTML = ""
 
 	$.ajax({
-	    url: "https://api.instagram.com/v1/tags/" + search + "/media/recent?access_token=1193616676.1677ed0.c2a52736eb714db3bef6b8671b485be2",
+	    url: "https://api.instagram.com/v1/tags/" + search + "/media/recent?access_token=ACCESS-TOKEN",
 	    jsonp: "callback",
 	    dataType: "jsonp",
-	    success: function(data) {
-	    	for(i = 0; i < data.data.length; i++) {
-	    		var newA = document.createElement('a')
-	    		newA.setAttribute('href', data.data[i].link)
-	    		newA.setAttribute('target', "_blank")
+	    success: function(response) {
+	    	var $instagram = $( '#instragram' );
+	    	$instagram.html('');
+	    	//var results = document.getElementById("results")
+	    	for(var i = 0; i < response.data.length; i++) {
+	    		imageUrl = response.data[i].images.low_resolution.url;
+	    		$instagram.append('<img src="' + imageUrl + '" />');
+	    		//var newA = document.createElement('a')
+	    		//newA.setAttribute('href', response.data[i].link)
+	    		//newA.setAttribute('target', "_blank")
 
-	    		var newDiv = document.createElement('div')
-	    		newDiv.className = "col-sm-3 col-sm-offset-1 con animated flipInY"
+	    		//var newDiv = document.createElement('div')
+	    		//newDiv.className = "col-sm-3 col-sm-offset-1 con animated flipInY"
 
-	    		var newImg = document.createElement('img')
-	    		newImg.setAttribute('src', data.data[i].images.thumbnail.url)
-	    		newDiv.appendChild(newImg)
+	    		//var newImg = document.createElement('img')
+	    		//newImg.setAttribute('src', response.data[i].images.thumbnail.url)
+	    		//newDiv.appendChild(newImg)
 
-	    		var newLike = document.createElement('h5')
-	    		var likes = document.createTextNode('Likes: ' + data.data[i].likes.count)
-	    		newLike.appendChild(likes)
-	    		newDiv.appendChild(newLike)
+	    		//var newLike = document.createElement('h5')
+	    		//var likes = document.createTextNode('Likes: ' + response.data[i].likes.count)
+	    		//newLike.appendChild(likes)
+	    		//newDiv.appendChild(newLike)
 
-	    		var creator = document.createElement('h5')
-	    		var createdBy = document.createTextNode('From: ' + data.data[i].caption.from.username)
-	    		creator.appendChild(createdBy)
-	    		newDiv.appendChild(creator)
-	    		newA.appendChild(newDiv)
+	    		//var creator = document.createElement('h5')
+	    		//var createdBy = document.createTextNode('From: ' + response.data[i].caption.from.username)
+	    		//creator.appendChild(createdBy)
+	    		//newDiv.appendChild(creator)
+	    		//newA.appendChild(newDiv)
 
-	    		document.getElementById('results').appendChild(newA)
+	    		//document.getElementById('results').appendChild(newA)
 	    	}
 	    },
 	    type: 'GET'
